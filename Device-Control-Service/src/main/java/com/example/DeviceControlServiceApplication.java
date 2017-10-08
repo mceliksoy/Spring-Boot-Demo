424174d670d966aa13343af9688d8a017f7c83f7
celiksoy <celiksoy@netas.com.tr>
2017-10-08 18:05:21 +0300

    Initial commit to demo project

---
+++ Device-Control-Service/src/main/java/com/example/DeviceControlServiceApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceControlServiceApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeviceControlServiceApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("The device control service is running .................");
+        SpringApplication.run(DeviceControlServiceApplication.class, args);
+    }
+}
---
+++ Gui-Service/src/main/java/com/example/GuiServiceApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiServiceApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GuiServiceApplication.class);
+	public static void main(String[] args) {
+		LOGGER.debug("The Gui service is running........");
+		SpringApplication.run(GuiServiceApplication.class, args);
+	}
+}
